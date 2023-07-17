e953871b2979fe14d8fa1d58cca76ebf110dd829
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-17 12:40:42 +0200

    added login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,17 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
-  email:string = '';
-  password:string = '';
+export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit():void {
+  ngOnInit(): void {
 
   }
 
-  login(){
-    
+  async login() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    const requestOptions:RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    try {
+    let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions);
+    let json = await resp.json();
+    localStorage.setItem('token', json.token);
+  } catch(e) {
+    // Show error message
+    console.error(e)
   }
+}
 }
