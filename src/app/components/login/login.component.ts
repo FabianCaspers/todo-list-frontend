fa422331a2674d65b3858b2e49aae1ff5494d2cb
FabianCaspers <fabian.caspers1308@gmail.com>
2023-07-17 19:53:52 +0200

    add auth service, clean login code

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -9,35 +13,27 @@ export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient, private as: AuthService) { }
 
   ngOnInit(): void {
 
   }
 
   async login() {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-      "username": this.username,
-      "password": this.password
-    });
-
-    const requestOptions:RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-
     try {
-    let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions);
-    let json = await resp.json();
-    localStorage.setItem('token', json.token);
-  } catch(e) {
-    // Show error message
-    console.error(e)
+      let resp = await this.as.loginWithUsernameAndPassword(this.username, this.password);
+      console.log(resp);
+    } catch (e) {
+      console.error(e)
+    }
   }
-}
+
+  public loginWithUsernameAndPassword(username: string, password: string) {
+    const url = environment.baseURL + '/login/';
+    const body = {
+      "username": username,
+      "password": password
+    }
+    return lastValueFrom(this.http.post(url, body));
+  }
 }
